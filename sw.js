45e8fc621eaebb3d51ce6d7510b9e7a4165bcb40
vagrant <deforddm@gmail.com>
2026-09-12 15:02:09 -0700

    v1.9.2: fix the swell LFO wiring that made ambient water audible everywhere, and quieten it

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guin's Garden service worker */
-const CACHE = 'guins-garden-v1.9.1';
+const CACHE = 'guins-garden-v1.9.2';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', e => {
