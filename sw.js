4479569d37e0534e001cf9c81f5e9044fc446fc3
vagrant <deforddm@gmail.com>
2026-09-11 19:00:06 -0700

    Terrarium bugs move about, indoors is full size, and the back of Guin's head has hair

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guin's Garden service worker */
-const CACHE = 'guins-garden-v3';
+const CACHE = 'guins-garden-v4';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', e => {
