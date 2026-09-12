95a7cea83a4f9b4773067ebfee8a1310c89d8ca2
vagrant <deforddm@gmail.com>
2026-09-11 17:43:02 -0700

    Fix: taps on menu buttons were being swallowed by the joystick handler

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guin's Garden service worker */
-const CACHE = 'guins-garden-v1';
+const CACHE = 'guins-garden-v2';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', e => {
