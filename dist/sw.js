34a4e9812a4a89d9fe15a60ee66760bb3cae1ffd
vagrant <deforddm@gmail.com>
2026-09-17 18:26:01 -0700

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
 /* Guin's Garden service worker */
-const CACHE = 'guins-garden-v1.13.0';
+const CACHE = 'guins-garden-v1.14.0';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', e => {
